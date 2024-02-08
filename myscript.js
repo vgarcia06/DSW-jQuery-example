ecec83145c7b8483858ef05eda3a3d1bf0262eb4
vgarcia06 <158102779+vgarcia06@users.noreply.github.com>
2024-02-08 13:48:06 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
-	<!-- $("h1").click(function() {
+	 $("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
-		$("img").css("border","25px solid black"); -->
+		$("img").css("border","25px solid black"); 
 	});
 });
