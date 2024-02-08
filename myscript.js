f3cebad8cc0a7ba18b23cb81faaf89c4b43f4b00
sky-adams <skyadams@sbunified.org>
2024-02-08 09:13:10 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
+	$("#imagetoggler").click(function(){
+		$("img").toggle();
+	});
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
-	});
-	$("#imagetoggler").click(function(){
-		$("img").toggle();
 	});
 });
